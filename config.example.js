98bb9e9ab6fa1acc185a74571dcdd9e22b60abeb
James Sumners <james.sumners@gmail.com>
2016-05-06 10:40:57 -0400

    Initial commit

---
+++ config.example.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  logger: {
+    name: 'ascmnml',
+    level: 'info'
+  },
+
+  mongoose: {
+    uri: 'mongodb://localhost/asc',
+    options: {}
+  },
+
+  server: {
+    connection: {
+      address: '127.0.0.1',
+      port: '9000'
+    }
+  },
+
+  soundcloud: {
+    clientId: 'not used right now'
+  }
+};
---
+++ templates/helpers/hearthis.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.shortUrlTrackId = function suti(url) {
+  return /\/([0-9]+)\//.exec(url)[1];
+};
