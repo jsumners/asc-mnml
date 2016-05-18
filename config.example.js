1ff6ee74a988eb42c804532f37b8083db762cd00
James Sumners <james.sumners@gmail.com>
2016-05-18 18:16:09 -0400

    Update to standardjs.com

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 module.exports = {
   logger: {
@@ -21,4 +21,4 @@ module.exports = {
   soundcloud: {
     clientId: 'not used right now'
   }
-};
+}
--- templates/helpers/youtube.js
+++ templates/helpers/youtube.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
-const URL = require('url');
+const URL = require('url')
 
-module.exports.getYouTubeId = function getYouTubeId(url) {
-  const _url = URL.parse(url, true);
-  return _url.query.v;
-};
+module.exports.getYouTubeId = function getYouTubeId (url) {
+  const _url = URL.parse(url, true)
+  return _url.query.v
+}
